472b5cbe018b7f8dfca90fc14d602b2f01ea83c4
Valerii <fokin051990@gmail.com>
2019-01-06 00:05:23 +0100

    omg fail

--- softplant/src/main/java/com/softwareplant/api/container/configuration/PersistenceConfiguration.java
+++ softplant/src/main/java/com/softwareplant/api/container/configuration/PersistenceConfiguration.java
@@ -1,12 +1,29 @@
 package com.softwareplant.api.container.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
 
 @Configuration
 @EnableJpaRepositories("com.softwareplant.api.module.**")
 @EntityScan("com.softwareplant.api.module.**")
+@EnableTransactionManagement
 @EnableJpaAuditing
-public class PersistenceConfiguration {}
+public class PersistenceConfiguration {
+
+    @Autowired
+    private EntityManagerFactory entityManagerFactory;
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+}
